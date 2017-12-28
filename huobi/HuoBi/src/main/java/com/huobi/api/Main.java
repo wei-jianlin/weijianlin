2850b5c8d9049ee87461118a18c6a6927acd368c
wei-jianlin <1065574027@qq.com>
2017-12-28 23:50:44 +0800

    异常捕获

--- huobi/HuoBi/src/main/java/com/huobi/api/Main.java
+++ huobi/HuoBi/src/main/java/com/huobi/api/Main.java
@@ -21,14 +21,13 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     
     public static void main(String[] args) {
-        try {
-            ApiClient client = new ApiClient();
-            while(true)allSymbolMin30(client);
-          } catch (ApiException e) {
-              logger.error("API Error! err-code: " + e.getErrCode() + ", err-msg: " + e.getMessage());
-          } catch (InterruptedException e) {
-              logger.error("Thread Error! err-msg: " + e.getMessage());
-        }
+        ApiClient client = new ApiClient();
+        while(true)
+            try {
+                allSymbolMin30(client);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
     }
     
     public static void allSymbolMin30(ApiClient client) throws InterruptedException{
@@ -46,48 +45,48 @@ public class Main {
         List<Kline> list = new ArrayList<Kline>(0);
         try{
            list = client.getHistoryKline(symbolEnum, Constant.MIN_30, 4);
+           Kline firstKline = null;
+           double riseAndFallTotal = 0;
+           for(int i = 0; i < list.size(); i++) {
+               firstKline = list.get(0);
+               Kline kline = list.get(i);
+               double riseAndFall = Arithmetic.riseAndFall(kline.open, kline.close);
+               riseAndFallTotal += riseAndFall; 
+               
+               logger.info(symbolEnum + " 30分钟线涨跌幅:" + 
+               Arithmetic.riseAndFallToString(Arithmetic.riseAndFall(kline.open, kline.close))
+               + "  " + calendar.getTime().toLocaleString());
+               
+               calendar.add(Calendar.MINUTE, -30);
+               if(riseAndFallTotal < Constant.BUY_RISE1_30) {
+                   String buyPrice = new BigDecimal(firstKline.close).setScale(8,BigDecimal.ROUND_DOWN)
+                           .toString();
+                   String salePrice = new BigDecimal(firstKline.close * (1 + Constant.SALE_RISE1_30))
+                           .setScale(8,BigDecimal.ROUND_DOWN).toString();
+                   Long buyOrderId = CreateOrder.orderPlace(client, buyPrice, symbolEnum, Constant.BUY_LIMIT);
+                   logger.info("-----买入价为" + buyPrice + "-----" + "卖出价:" + salePrice);
+                   //挂单一分钟
+                   Thread.sleep(60000);
+                   OrderDetails orderDetails = client.orderDetail(buyOrderId.toString());
+                   //部分成交或者完全成交
+                   switch(orderDetails.state){
+                   case "partial-filled":
+                       CreateOrder.submitcancel(client, buyOrderId.toString());
+                       CreateOrder.orderPlace(client, salePrice, symbolEnum, Constant.SELL_LIMIT);
+                       break;
+                   case "filled":
+                       CreateOrder.orderPlace(client, salePrice, symbolEnum, Constant.SELL_LIMIT);
+                       break;
+                   default:
+                       CreateOrder.submitcancel(client, buyOrderId.toString());
+                   }
+                   break;
+               }
+           }
         }catch(ApiException e){
             logger.error("API Error! err-code: " + e.getErrCode() + ", err-msg: " + e.getMessage()
                     + ",paramter:" + symbolEnum);
         }
-        Kline firstKline = null;
-        double riseAndFallTotal = 0;
-        for(int i = 0; i < list.size(); i++) {
-            firstKline = list.get(0);
-            Kline kline = list.get(i);
-            double riseAndFall = Arithmetic.riseAndFall(kline.open, kline.close);
-            riseAndFallTotal += riseAndFall; 
-            
-            logger.info(symbolEnum + " 30分钟线涨跌幅:" + 
-            Arithmetic.riseAndFallToString(Arithmetic.riseAndFall(kline.open, kline.close))
-            + "  " + calendar.getTime().toLocaleString());
-            
-            calendar.add(Calendar.MINUTE, -30);
-            if(riseAndFallTotal < Constant.BUY_RISE1_30) {
-                String buyPrice = new BigDecimal(firstKline.close).setScale(8,BigDecimal.ROUND_DOWN)
-                        .toString();
-                String salePrice = new BigDecimal(firstKline.close * (1 + Constant.SALE_RISE1_30))
-                        .setScale(8,BigDecimal.ROUND_DOWN).toString();
-                Long buyOrderId = CreateOrder.orderPlace(client, buyPrice, symbolEnum, Constant.BUY_LIMIT);
-                //挂单一分钟
-                Thread.sleep(60000);
-                OrderDetails orderDetails = client.orderDetail(buyOrderId.toString());
-                //部分成交或者完全成交
-                switch(orderDetails.state){
-                case "partial-filled":
-                    CreateOrder.submitcancel(client, buyOrderId.toString());
-                    CreateOrder.orderPlace(client, salePrice, symbolEnum, Constant.SELL_LIMIT);
-                    break;
-                case "filled":
-                    CreateOrder.orderPlace(client, salePrice, symbolEnum, Constant.SELL_LIMIT);
-                    break;
-                default:
-                    CreateOrder.submitcancel(client, buyOrderId.toString());
-                }
-                logger.info("-----买入价为" + buyPrice + "-----" + "卖出价:" + salePrice);
-                break;
-            }
-        }
-    }
-    
+        
+    }  
 }
