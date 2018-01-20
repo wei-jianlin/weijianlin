b72433abb08ba41dcff3f61a6fd7420b417ab4c6
wei-jianlin <1065574027@qq.com>
2018-01-20 17:21:19 +0800

    webpack-demo

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,11 +1,21 @@
-import _ from 'lodash'
-function component() {
-  var element = document.createElement('div');
+ import _ from 'lodash';
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ function component() {
+   var element = document.createElement('div');
+   var button = document.createElement('button');
+   var br = document.createElement('br');
+   button.innerHTML = 'Click me and look at the console!';
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.appendChild(br);
+   element.appendChild(button);
 
-  return element;
-}
+   // Note that because a network request is involved, some indication
+   // of loading would need to be shown in a production-level site/app.
+   button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+     var print = module.default;
 
-document.body.appendChild(component());
+     print();
+   });
+   return element;
+ }
+ document.body.appendChild(component());
