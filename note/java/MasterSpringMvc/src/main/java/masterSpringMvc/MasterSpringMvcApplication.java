e4fff0dbc33cdaf64eeff4f763f5b7bf3e9a33f0
wei-jianlin <1065574027@qq.com>
2018-04-15 16:38:33 +0800

    s

--- note/java/MasterSpringMvc/src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
+++ note/java/MasterSpringMvc/src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
@@ -3,6 +3,12 @@ package masterSpringMvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * @SpringBootApplication注解实际上组合了3个其他的注解:
+ * @Configuration:该类处理spring的常规配置.
+ * @EnableAutoConfiguration:springBoot自动配置,
+ * @ComponentScan:用注解标识的类 会被spring自动扫描并且装入bean容器
+ * */
 @SpringBootApplication
 public class MasterSpringMvcApplication {
 
--- note/java/MasterSpringMvc/src/main/java/masterSpringMvc/controller/HelloController.java
+++ note/java/MasterSpringMvc/src/main/java/masterSpringMvc/controller/HelloController.java
@@ -1,10 +1,14 @@
 package masterSpringMvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    @RequestBody
+    @ResponseBody
     public String hello(){
         return "hello world";
     }
